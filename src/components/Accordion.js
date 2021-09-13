95659f4de51a96fd25860747f837f3fb3f0790f3
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-09-13 15:25:32 -0400

    fixing warning

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -70,11 +70,11 @@ const Accordion = ({ items }) => {
     };
   
     const renderedItems = items.map((item, index) => {
-        const active = index === activeIndex ? 'active' : '';
-
+      const active = index === activeIndex ? 'active' : '';
+  
       return (
         <React.Fragment key={item.title}>
-              <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
             <i className="dropdown icon"></i>
             {item.title}
           </div>
@@ -85,12 +85,7 @@ const Accordion = ({ items }) => {
       );
     });
   
-    return (
-      <div className="ui styled accordion">
-        {renderedItems}
-        
-      </div>
-    );
+    return <div className="ui styled accordion">{renderedItems}</div>;
   };
   
   export default Accordion;
--- src/components/Search.js
+++ src/components/Search.js
@@ -27,38 +27,111 @@ import axios from 'axios';
 // what does <span dangerouslySetInnerHTML={{__html do?
 // anytime you take a string from a 3rd party. you could be introducing a security hole
 // an xxs attack or X site scripting attack, it could allow hackers or malicious person into app
+// ---------------------------------------------------------------//
 
+// we want to createa button that links us to the desc of the 
+// article that was searched
+// we would need a <div> and an <a> (anchor)
+// we want to fix an error where it errors out when we delete our search
+// it does this due to the fact that we are doing a search with an empit string
+// restore it by giving it an if statement of term
+// ---------------------------------------------------------------//
 
+// we want to shorten the api request to only search for something every 500 ms
+// we want to cancel the previous timer fpr every input change
+// the results were delayted and needsa work around
+// you would throw a couple of checks into our hook. We must detect whether
+// or not its the first time we are going to skjip doing any time out
+// and do a search immediately. for every additional type or useEffect being called
+// we will go ahead and set a timeout and return the function(check line 68) || line containing the firs tif statement
+// it allows first render of component
 
+// ---------------------------------------------------------------//
+
+// figutre out wht this error message means React Hook useEffect has a 
+// missing dependency: 'results.length'.Either include it or remove the 
+// dependency array  react - hooks / exhaustive - deps overrideMethod
+// this next video is advanced and will be hard to understand. This preview will be
+// naturally convered later 
+// when ususing useEffect you wanna decalre some prop or component
+// anytime you use a prop or state inside useEffect. React has a rule wants you to reference
+// any peice of propo or state in a dependency array. look at [term]
+// use reulsts.length it helps you avoid useEffect issues
+// to solve the error bug we got from solving our other error bug
+// introduce debouncedTerm which will be a time lagged search term
+// in the new code we have one useEffect that is goign to run anytime term chagnes
+// term will change anytime a user intput something
+// anytime second useEffect changes we are going to que a change to debounc term
+// thatll execute in 1 second. If use changes term again we will clear timeout
+// and set up timer. Whenever a change in set eboucne term is processed we will run
+// seoncd useEffect we put together. Whenever another useEffect is called it take useEffect
+// itll update key reults of state.
+// ---------------------------------------------------------------//
 
 const Search = () => {
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
-  
+
     useEffect(() => {
-      const search = async () => {
-        const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-          params: {
-            action: 'query',
-            list: 'search',
-            origin: '*',
-            format: 'json',
-            srsearch: term,
-          },
-        });
-  
-        setResults(data.query.search);
-      };
-  
-      search();
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+        return() => {
+            clearTimeout(timerId)
+};
     }, [term]);
+
+    useEffect(() => {
+            const search = async (search) => {
+              const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                  action: 'query',
+                  list: 'search',
+                  origin: '*',
+                  format: 'json',
+                  srsearch: debouncedTerm,
+                },
+              });
+        
+              setResults(data.query.search);
+            };
+            search();
+    }, [debouncedTerm]);
+  
+    // useEffect(() => {
+    //     if (term && !results.length) {
+    //         search();
+    //     } else {
+    //         const timeoutId = setTimeout(() => {
+    //             if (term) {
+    //               search();
+    //             }
+    //           }, 1000);
+                
+    //             return () => {
+    //                 clearTimeout(timeoutId)
+        
+    //             };
+    //     }
+  
+      
+    // }, [term, results.length]);
   
     const renderedResults = results.map((result) => {
       return (
         <div key={result.pageid} className="item">
+          <div className="right floated content">
+            <a
+              className="ui button"
+              href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            >
+              Go
+            </a>
+          </div>
           <div className="content">
-                  <div className="header">{result.title}</div>
-                  <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+            <div className="header">{result.title}</div>
+            <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
           </div>
         </div>
       );
