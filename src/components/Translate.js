9b5a4e7af35a2c535c91edeb1fab2a4b12158617
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-09-14 00:31:19 -0400

    debouncing translation

---
+++ src/components/Translate.js
@@ -0,0 +1,71 @@
+// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+import Convert from './Convert';
+
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    },
+    {
+        label: 'Arabic',
+        value: 'ar'
+    },
+    {
+        label: 'Hindi',
+        value: 'hi'
+    },
+    {
+        label: 'Dutch',
+        value: 'nl'
+    },
+    {
+        label: 'Portuguese(Portugal)',
+        value: 'pt-PT'
+    },
+    {
+        label: 'Spanish',
+        value: 'es'
+    },
+    {
+        label: 'Chinese(Taiwan)',
+        value: 'zh-TW'
+    },
+    {
+        label: 'Japanese',
+        value: 'ja'
+    },
+    {
+        label: 'Korean',
+        value: 'ko'
+    }
+];
+
+const Translate = () => {
+    const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('')
+    return (
+        <div> 
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                    </div>
+                </div>
+            <Dropdown
+                label="Select a Language"
+                selected={language}
+                onSelectedChange={setLanguage}
+                options={options}
+            />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language}/>
+        </div>
+
+    );
+
+};
+
+export default Translate
